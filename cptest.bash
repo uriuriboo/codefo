4be494a6e70762d305d330e8d7b0efccf07b6265
uriuriboo <65266760+uriuriboo@users.noreply.github.com>
2021-03-21 16:02:52 +0900

    Add files via upload

--- cptest.bash
+++ cptest.bash
@@ -1,8 +1,7 @@
 #!/bin/bash
-contest="abc"
 problemname=$1
 problem=$2
 if [ ! -e test/${contest}${problemname}_${problem} ];then
-    oj d https://codeforces.com/contest/${problemname}/problem/${problem} -d test/${contest}${problemname}_${problem}
+    oj d https://codeforces.com/contest/${problemname}/problem/${problem} -d test/${problemname}${problem}
 fi
-oj test  -c ./problem.exe ./test/${contest}$1_$2/* 
+oj test  -c ./problem.exe ./test/$1$2/* 
